591b9f1b2ff7870f1384974849b57b19970ca365
Joao Loboo <joao.neto@hp.com>
2022-08-18 21:09:52 -0300

    Project DevOps CI/CD

---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+#resource "aws_key_pair" "challange_DevOps" {
+# 
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvpJkcQjpiXpz7BQCFK64dhQp6wh7OM8wCFkQR3p4G1EaJghbtdUtfEfN1FcU/DDQihWRxzGhx8YbowG6xYXv0vb0ODD1lbKx6q5eQkTyfFGCL56cPjZpd5gCG2aEu4+57E4cQQbPEwOAqhdU/EqmJf3BUdiX/06UbGvC2jMrJUfjZUbs1mKtSqryG9n/gi/21gMg2MYGvS0uUOmiEcVMyAh+fSfqYaWCACGdiaNSeQWCn9QSwYLACO6fmBJAKDyNa3s0zMBq8mO2B8fKJvoscyTe1u/fN0ddM/jkWkqByXLhNESQ04ZNDkKkFAHOv/87khhLZ1D0lXKZWFl9B0kjV teste_infra"  
+#}
+
+resource "aws_instance" "challenge_DevOps_Machine" {
+  ami = "ami-042e8287309f5df03"
+  instance_type = "t2.micro"
+  key_name = "challange_DevOps"
+    provisioner "remote-exec" {
+  inline = [
+    "echo \"SSH Connection is OK\""
+
+  ]
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    port        = 22
+    user        = "ubuntu"
+    private_key = file("/home/root/.ssh/id_rsa")
+    agent       = false
+
+  }
+}
+  tags = {
+    Name = "machine01_DevOps"
+  }
+  vpc_security_group_ids = [aws_security_group.allow-acess.id]
+}
+
+resource "null_resource" "run" {
+  provisioner "local-exec" {
+    command = "echo EC2 Ok"
+  }
+  depends_on = [aws_instance.challenge_DevOps_Machine]
+}
---
+++ infra/security-group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow-acess" {
+  name        = "allow-acess"
+  description = "allow-acess"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "${var.cdirs_acesso_remoto}"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "${var.cdirs_acesso_remoto}"
+  }
+  tags = {
+    Name = "allow_access"
+  }
+}
+
+
